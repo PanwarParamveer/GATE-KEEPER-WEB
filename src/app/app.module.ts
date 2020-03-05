cdf559f5c6c5a8d056588145968a8a92bc793f35
Paramveer Panwar <panwarparma11@gmail.com>
2020-03-05 11:20:14 +0530

    update

---
+++ src/app/app.module.ts
@@ -0,0 +1,29 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import {LayoutModule} from './modules/layout/layout.module';
+
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { environment } from 'src/environments/environment';
+import { FormsModule } from '@angular/forms';
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    LayoutModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireAuthModule,
+    AngularFirestoreModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/modules/auth/register/register.component.ts
@@ -0,0 +1,37 @@
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import { AuthServiceService } from '../auth-service.service';
+
+declare var $;
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.scss']
+})
+export class RegisterComponent implements OnInit, OnDestroy {
+
+  authError: any;
+
+  constructor(private auth: AuthServiceService  ) {
+  }
+
+  ngOnInit() {
+    $('body').addClass('hold-transition login-page');
+    $(() => {
+      $('input').iCheck({
+        checkboxClass: 'icheckbox_square-blue',
+        radioClass: 'iradio_square-blue',
+        increaseArea: '20%' /* optional */
+      });
+    });
+  }
+
+  ngOnDestroy(): void {
+    $('body').removeClass('hold-transition login-page');
+  }
+
+  createUser(frm) {
+    this.auth.createUser(frm.value);
+  }
+
+}
